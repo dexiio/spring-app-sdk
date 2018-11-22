5fbb3c6beb81042c6a9acc3b60bdc11be04d9e33
Morten Garbøl Franck <mgf@dexi.io>
2018-11-22 16:59:16 +0100

    DEXI-2594: removed unused code.

--- src/main/java/io/dexi/service/handlers/DataStorageHandler.java
+++ src/main/java/io/dexi/service/handlers/DataStorageHandler.java
@@ -4,30 +4,4 @@ import io.dexi.service.Rows;
 
 public interface DataStorageHandler<T, U> {
     default Object write(String activationId, T activationConfig, String componentId, U componentConfig, Rows rows) { return false; }
-
-    //Class<U> getDataStoragePayloadClass();
-
-    /*
-    abstract class AbstractDataStoragePayload<V> {
-        private V config;
-
-        private Rows rows = new Rows();
-
-        public V getConfig() {
-            return config;
-        }
-
-        public void setConfig(V config) {
-            this.config = config;
-        }
-
-        public Rows getRows() {
-            return rows;
-        }
-
-        public void setRows(Rows rows) {
-            this.rows = rows;
-        }
-    }
-    */
 }

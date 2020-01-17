ff5ed5dc04f122ce28018c6f531976fad8670c0b
Henrik von Hofmeister <hh@cloudscrape.com>
2020-01-17 12:02:13 +0100

    Added helper for streaming request bodies

---
+++ src/main/java/io/dexi/service/utils/RequestUtils.java
@@ -0,0 +1,22 @@
+package io.dexi.service.utils;
+
+import io.dexi.client.StreamingRequestBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class RequestUtils {
+
+    public RequestBody createStreamingBody(HttpServletRequest request) throws IOException {
+        final MediaType mediaType = MediaType.parse(request.getContentType());
+        return new StreamingRequestBody(mediaType, request.getInputStream());
+    }
+
+    public MultipartBody.Part createStreamingPart(HttpServletRequest request) throws IOException {
+        final MediaType mediaType = MediaType.parse(request.getContentType());
+        return MultipartBody.Part.create(new StreamingRequestBody(mediaType, request.getInputStream()));
+    }
+}

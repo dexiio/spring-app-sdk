83a3064bee16947996bd4d918d2ad08365e84fc4
Henrik von Hofmeister <hh@cloudscrape.com>
2020-02-13 15:20:15 +0100

    DEXI-862: Added message-only constructor

--- src/main/java/io/dexi/service/exceptions/ActivationException.java
+++ src/main/java/io/dexi/service/exceptions/ActivationException.java
@@ -1,6 +1,11 @@
 package io.dexi.service.exceptions;
 
 public class ActivationException extends Exception {
+
+    public ActivationException(String message) {
+        super(message);
+    }
+
     public ActivationException(String message, Throwable cause) {
         super(message, cause);
     }
